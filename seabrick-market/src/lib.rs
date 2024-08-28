599ace6ad0d81da4f53e51d46f69afe1ce29774a
NanezX <victor.nanezj@gmail.com>
2024-08-28 19:49:04 -0400

    update error and event management in market

--- seabrick-market/src/lib.rs
+++ seabrick-market/src/lib.rs
@@ -20,7 +20,6 @@ static ALLOC: mini_alloc::MiniAlloc = mini_alloc::MiniAlloc::INIT;
 
 sol_interface! {
     interface ISeabrick {
-        function totalSupply() external view returns (uint256);
         function mint(address to) external;
     }
 
@@ -40,39 +39,39 @@ sol_interface! {
 }
 
 sol! {
-    event AggregatorAdded(bytes32 indexed name, address indexed aggregator, address indexed token);
+    /// A chainlink aggregator added
+    event AggregatorAdded(bytes32 name, address aggregator, address token);
 
     /// Emitted when contract sell a NFT
-    event NFTSold(address indexed owner, uint256 indexed id);
+    event Buy(address buyer);
 
-    event TokenClaimed(address indexed token, uint256 indexed amount);
+    /// Tokens claimed
+    event Claimed(address token, uint256 amount);
 }
 
 sol! {
     /// NFT not bought
-    error NotBought();
-    /// Mismatch on data provided
-    error MismatchData();
+    error PaymentFailed();
 
-    /// Error from a call
-    error CallError(uint256 id);
+    /// Mismatch on aggregators data provided
+    error MismatchAggregators();
 
     /// Error when claiming
-    error NotClaimed();
+    error ClaimFailed();
 }
 
 #[derive(SolidityError)]
 pub enum MarketError {
-    NotBought(NotBought),
-    MismatchData(MismatchData),
-    CallError(CallError),
-    NotClaimed(NotClaimed),
+    PaymentFailed(PaymentFailed),
+    MismatchAggregators(MismatchAggregators),
+    ClaimFailed(ClaimFailed),
 }
 
 sol_storage! {
     pub struct AggregatorInfo {
         /// The chainlink oracle aggregator address for the given token
         address agregator_address;
+
         /// Token that will be used as payment and that correspond to the oracle Token/USD
         /// If token address is a non-zero address, it will try to be used to make transfers.
         /// If this token address is a zero address, it will assume that is native currency (like ETH in Arbitrum One)
@@ -113,14 +112,12 @@ impl Market {
         names: Vec<FixedBytes<32>>,
         agregators: Vec<Address>,
         tokens: Vec<Address>,
-    ) -> Result<(), MarketError> {
+    ) -> Result<(), Vec<u8>> {
         // Check if already init. Revert if already init
-        if let Err(_) = self.init._check_init() {
-            return Err(MarketError::CallError(CallError { id: U256::from(1) }));
-        }
+        self.init._check_init()?;
 
-        // Set contract owner using the initializer deployer
-        self.ownable._owner.set(msg::sender());
+        // Set contract owner/claimer using the initializer deployer
+        self.ownable._transfer_ownership(msg::sender());
 
         // Set NFT price
         self.price.set(price);
@@ -130,7 +127,7 @@ impl Market {
 
         // Set agregators info
         if names.len() != agregators.len() || names.len() != tokens.len() {
-            return Err(MarketError::MismatchData(MismatchData {}));
+            return Err(MarketError::MismatchAggregators(MismatchAggregators {}).into());
         }
 
         for i in 0..names.len() {
@@ -146,36 +143,22 @@ impl Market {
         }
 
         // Change contract state to already initialized
-        self.init.is_init.set(true);
+        self.init._set_init(true);
 
         Ok(())
     }
 
-    pub fn buy(&mut self, buyer: Address, name: FixedBytes<32>) -> Result<(), MarketError> {
+    pub fn buy(&mut self, buyer: Address, name: FixedBytes<32>) -> Result<(), Vec<u8>> {
         let payment_token = IERC20::new(self.price_feeds.get(name).token.get());
         let oracle = AggregatorV3Interface::new(self.price_feeds.get(name).agregator_address.get());
 
         // Get latest answer price
-        let latest_answer = match oracle.latest_round_data(Call::new_in(self)) {
-            Ok(data) => U256::from_limbs(data.1.into_limbs()),
-            Err(_) => {
-                return Err(MarketError::CallError(CallError { id: U256::from(2) }));
-            }
-        };
+        let latest_answer =
+            U256::from_limbs(oracle.latest_round_data(Call::new_in(self))?.1.into_limbs());
 
-        let oracle_decimals = match oracle.decimals(Call::new_in(self)) {
-            Ok(data) => U256::from(data),
-            Err(_) => {
-                return Err(MarketError::CallError(CallError { id: U256::from(3) }));
-            }
-        };
+        let oracle_decimals = U256::from(oracle.decimals(Call::new_in(self))?);
 
-        let payment_decimals = match payment_token.decimals(Call::new_in(self)) {
-            Ok(data) => U256::from(data),
-            Err(_) => {
-                return Err(MarketError::CallError(CallError { id: U256::from(4) }));
-            }
-        };
+        let payment_decimals = U256::from(payment_token.decimals(Call::new_in(self))?);
 
         // Scaled price
         let usd_price = self.price.get()
@@ -184,72 +167,52 @@ impl Market {
 
         let amount_need = usd_price.div_ceil(latest_answer);
 
-        let success = match payment_token.transfer_from(
+        let success = payment_token.transfer_from(
             Call::new_in(self),
             buyer,
             contract::address(),
             amount_need,
-        ) {
-            Ok(data) => data,
-            Err(_) => {
-                return Err(MarketError::CallError(CallError { id: U256::from(5) }));
-            }
-        };
-
+        )?;
         if !success {
-            return Err(MarketError::NotBought(NotBought {}).into());
+            return Err(MarketError::PaymentFailed(PaymentFailed {}).into());
         }
 
         // Increasing the total collected by this payment token
-        let collected = self.total_collected.get(payment_token.address).clone();
+        let collected = self.total_collected.get(payment_token.address);
         self.total_collected
             .setter(payment_token.address)
             .set(collected + amount_need);
 
         let seabrick = ISeabrick::new(self.nft_token.get());
 
-        let new_id = match seabrick.total_supply(Call::new_in(self)) {
-            Ok(data) => data,
-            Err(_) => {
-                return Err(MarketError::CallError(CallError { id: U256::from(6) }));
-            }
-        };
+        // Mint the token to the buyer address
+        seabrick.mint(Call::new_in(self), buyer)?;
 
-        if let Err(_) = seabrick.mint(Call::new_in(self), buyer) {
-            return Err(MarketError::CallError(CallError { id: U256::from(7) }));
-        }
-
-        evm::log(NFTSold {
-            owner: buyer,
-            id: new_id,
-        });
+        evm::log(Buy { buyer });
 
         Ok(())
     }
 
-    pub fn claim(&mut self, name: FixedBytes<32>) -> Result<(), MarketError> {
-        if let Err(_) = self.ownable.only_owner() {
-            return Err(MarketError::CallError(CallError { id: U256::from(1) }));
-        }
+    pub fn claim(&mut self, name: FixedBytes<32>) -> Result<(), Vec<u8>> {
+        self.ownable.only_owner()?;
 
         let claim_token = IERC20::new(self.price_feeds.get(name).token.get());
-        let collected = self.total_collected.get(claim_token.address).clone();
+        let amount_collected = self.total_collected.get(claim_token.address);
 
         // Decreasing the total collected by this payment token to ZERO since everything was claimed
         self.total_collected
             .setter(claim_token.address)
             .set(U256::ZERO);
 
-        let success = match claim_token.transfer(Call::new_in(self), msg::sender(), collected) {
-            Ok(data) => data,
-            Err(_) => {
-                return Err(MarketError::CallError(CallError { id: U256::from(2) }));
-            }
-        };
-
+        let success = claim_token.transfer(Call::new_in(self), msg::sender(), amount_collected)?;
         if !success {
-            return Err(MarketError::NotClaimed(NotClaimed {}));
+            return Err(MarketError::ClaimFailed(ClaimFailed {}).into());
         }
+
+        evm::log(Claimed {
+            token: claim_token.address,
+            amount: amount_collected,
+        });
 
         Ok(())
     }
