f4c01fa50f3d336e25fb5da58f989c43deb85b70
NanezX <victor.nanezj@gmail.com>
2024-11-06 02:19:32 -0400

    adding support to change the ownership address saved on the contracts

--- seabrick-market/src/ownable.rs
+++ seabrick-market/src/ownable.rs
@@ -24,12 +24,19 @@ sol! {
     #[derive(Debug)]
     #[allow(missing_docs)]
     error OwnableUnauthorizedAccount(address account);
+
+    /// * `ownership_contract` - Address that's not allowed to become the ownership contract.
+    #[derive(Debug)]
+    #[allow(missing_docs)]
+    error InvalidOwnership(address ownership_contract);
 }
 
 #[derive(SolidityError, Debug)]
 pub enum OwnableError {
     /// The caller account is not authorized to perform an operation.
     UnauthorizedAccount(OwnableUnauthorizedAccount),
+    /// The ownership address is not a valid ownership contract
+    InvalidOwnership(InvalidOwnership),
 }
 
 sol_storage! {
@@ -45,6 +52,28 @@ impl Ownable {
     pub fn owner(&self) -> Result<Address, Vec<u8>> {
         let owner_address = Ownership::new(self._ownership.get()).owner(Call::new())?;
         Ok(owner_address)
+    }
+
+    pub fn change_ownership_contract(&mut self, new_address: Address) -> Result<(), Vec<u8>> {
+        self.only_owner()?;
+
+        // We check the owner on target contract to avoid losing the ownership
+        let target_owner_address = Ownership::new(new_address).owner(Call::new())?;
+
+        // If target ownership address is not a contract, it will fail the transaction
+        // If target ownership address owner is not the same that the current one
+        //    we fail the transaction to avoid losing the ownership of the contracts
+        if target_owner_address != self.owner()? {
+            return Err(OwnableError::InvalidOwnership(InvalidOwnership {
+                ownership_contract: target_owner_address,
+            })
+            .into());
+        }
+
+        // Change the ownership contract address
+        self._ownership.set(new_address);
+
+        Ok(())
     }
 }
 
